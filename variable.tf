da87c165b1b2b083eae6b6099928cf181124247b
MZC01-MYNECK <mzc01-myneck@MZC01-MYNECK.local>
2023-10-20 20:19:23 +0900

    var default add

--- variable.tf
+++ variable.tf
@@ -10,22 +10,27 @@ variable "password_sender" {
 }
 
 variable "db_name" {
+  default = "golfzondb"
   description = "Unique name to assign to RDS instance"
 }
 
 variable "db_username" {
+  default = "golfzonuser"
   description = "RDS root username"
 }
 
 variable "db_password" {
+  default = "golfzonpass"
   description = "RDS root user password"
   sensitive   = true
 }
 
 variable "multi_az" {
+  default = true
   description = "Multi AZ Select"
 }
 
 variable "apply_immediately" {
+  default = true
   description = "Specifies whether any database modifications are applied immediately"
 }
